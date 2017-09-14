09fe349288c29b8305e1034166a52e95c28ed6ef
hungnm2904 <hungnm2904@gmail.com>
2017-09-14 20:31:36 +0700

    Fix *.d.ts file

--- index.d.ts
+++ index.d.ts
@@ -1,23 +1,23 @@
-import * as mongoose from 'mongoose';
+import { Document, Model, Schema, Mongoose, NativeError } from 'mongoose';
 declare class TinyMongooseSchemas<
   T,
-  DocType extends mongoose.Document,
-  ModelType extends mongoose.Model<DocType>
+  DocType extends Document,
+  ModelType extends Model<DocType>
 > {
   private _schema;
-  constructor(schema: mongoose.Schema);
-  readonly schema: mongoose.Schema;
-  static from<
-    T,
-    DocType extends mongoose.Document,
-    ModelType extends mongoose.Model<DocType>
-  >(fields: any): TinyMongooseSchemas<T, DocType, ModelType>;
+  private _mongoose;
+  constructor(schema: Schema, mongoose: Mongoose);
+  readonly schema: Schema;
+  static from<T, DocType extends Document, ModelType extends Model<DocType>>(
+    fields: any,
+    mongoose: Mongoose
+  ): TinyMongooseSchemas<T, DocType, ModelType>;
   toModel(name: string): ModelType;
   pre(
     action: string,
     funcs:
       | ((arg) => Promise<T>)[]
-      | ((next: (err?: mongoose.NativeError) => void) => void)
+      | ((next: (err?: NativeError) => void) => void)
   ): void;
   methods(name: string, func: any): void;
   statics(name: string, func: any): void;
